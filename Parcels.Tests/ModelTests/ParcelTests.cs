e4f20836ecc49cc2012d005d503a50c672a7c22d
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-05-19 13:55:30 -0700

    initiate  project repo, creating tests for parcel.cs file

--- Parcels.Tests/ModelTests/ParcelTests.cs
+++ Parcels.Tests/ModelTests/ParcelTests.cs
@@ -14,5 +14,22 @@ namespace Shipment.Test
       Parcel newParcel = new Parcel(12, 12, 10, 10);
       Assert.AreEqual(typeof(Parcel), newParcel.GetType());
     }
+  
+    [TestMethod]
+    public void GetVolume_VolumeReturnsVolume_Double()
+    {
+      Parcel newParcel = new Parcel(12, 12, 10, 10);
+      double volume = newParcel.GetVolume();
+
+      Assert.AreEqual(1440, volume);
+    }
+    [TestMethod]
+    public void ParcelConstructor_GetCostToShip_ReturnsShippingCost()
+    {
+      Parcel newParcel = new Parcel(12, 12, 10, 10);
+      double cost = newParcel.GetCostToShip();
+
+      Assert.AreEqual(14.4, cost);
+    }
   }
 }
